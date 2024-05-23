f301226bdd2e16915928153974be79d628494f8d
Meryembadaoui55 <93876670+Meryembadaoui55@users.noreply.github.com>
2024-05-23 15:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import streamlit as st
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms import HuggingFacePipeline
+from langchain.document_loaders import PyPDFLoader, OnlinePDFLoader
 
 from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
