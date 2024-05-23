937342076a89271ec68ffd8aa7e6505dfe5270cb
Meryembadaoui55 <93876670+Meryembadaoui55@users.noreply.github.com>
2024-05-23 16:10:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ import transformers
 from huggingface_hub import login
 login(token=st.secrets["HF_TOKEN"])
 
-loader = PyPDFLoader("https://github.com/Meryembadaoui55/methodology-assistant/blob/main/test-1.pdf")
+loader = PyPDFLoader("Meryembadaoui55/methodology-assistant/blob/main/test-1.pdf")
 
 data = loader.load()
 
